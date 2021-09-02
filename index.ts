a7215be7a6dffb7203aa7ccc97840441a191f0bb
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-09-02 21:45:25 +0100

    🏷️ Fix types of custom loader

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ interface L10nArgs {
     localesDirectory?: string,
 
     filter: (...args: any[]) => string
-    loader?: ({ locales, defaultLocale, localesDirectory }: Partial<any>) => Promise<Record<string, string>> | Record<string, string>
+    loader?: (args: { locales?: string[], defaultLocale?: string, localesDirectory?: string }) => Promise<Record<string, string>> | Record<string, string>
 }
 
 export class L10n {
